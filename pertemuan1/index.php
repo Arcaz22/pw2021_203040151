9ed6a3b352dc252296141404bd1cdc416a4e74b4
Arcaz22 <chandraarcychan@gmail.com>
2021-02-07 00:13:19 +0700

    first commit

---
+++ pertemuan1/index.php
@@ -0,0 +1,11 @@
+<?php
+    echo "Hello Word";
+?>
+
+/* 
+    Chandra Arcychan Azfar
+    203040151
+    github.com/Arcaz22/pw2021_203040151
+    Pertemuan 2( 6 Februari 2021 )
+    Materi Minggu ini tentang Instalasi VCS, XAMPP
+*/
