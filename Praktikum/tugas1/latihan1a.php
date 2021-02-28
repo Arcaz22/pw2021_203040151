22b576df4a172cb5c503d0ab34649dc38787dac4
Arcaz22 <chandraarcychan@gmail.com>
2021-02-28 21:49:55 +0700

    latihan dan tugas praktikum

---
+++ Praktikum/tugas1/latihan1a.php
@@ -0,0 +1,14 @@
+<?php
+/*
+    Chandra Arcychan Azfar
+    203040151
+    Jumat,13.00
+*/
+?>
+<?php
+
+for ($i = 1; $i <= 3; $i++) {
+    for ($j = 1; $j <= 3; $j++) {
+        echo "Ini perulangan ke ($i,$j) <br>";
+    }
+}
